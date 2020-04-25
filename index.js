02927fc850e365bc763a30fcbe1ea84007da25ff
G Bilge Dal <lagala999@gmail.com>
2020-04-25 08:45:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, st){
+  return drivers.filter(n => {
+    if(st.toUpperCase()=== n.name.toUpperCase()){
+      return n;
+    }
+    
+  })
+  
+}
+
+function matchName(drivers, st){
+  return drivers.filter()
+
+  
+}
+function fuzzyMatch(drivers, st){
+
+  
+}
